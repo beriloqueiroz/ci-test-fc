4485a81cdacaec3bcd5f584505d04579230a8162
berilo queiroz <berilo.queiroz@gmail.com>
2025-01-13 15:47:34 -0300

    teste docker

--- math_test.go
+++ math_test.go
@@ -4,9 +4,9 @@ import "testing"
 
 func TestSoma(t *testing.T) {
 
-	total := soma(15, 15)
+	total := soma(10, 15)
 
-	if total != 30 {
+	if total != 25 {
 		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 30)
 	}
 }
